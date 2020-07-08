43c263e5b6fb9ddd2d9984f8c28712ac1c223df6
Suhyeon6801 <k0s3y0@naver.com>
2020-07-08 16:34:27 +0900

    EJS & Ajax 실습

--- 200701finproject/expressServer.js
+++ 200701finproject/expressServer.js
@@ -1,8 +1,36 @@
 const express = require("express")
 const app = express()
+
+app.set("views", __dirname+"/views");
+app.set("view engine","ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
  
 app.get('/', function (req, res) {
   res.send('Hello World');
+});
+
+app.get("/ejsTest", function(req,res){
+    res.render('test');
+});
+
+app.get("/ejsTable", function(req,res){
+    console.log("table test");
+    res.render('testTable');
+});
+
+app.post("/ajaxTest", function(req,res){
+    var userID = req.body.sendUserID;
+    var userPW = req.body.sendUserPW;
+    var userName = req.body.sendUserName;
+    var userEmail = req.body.sendUSerEmail;
+
+    console.log("요청 바디 : ", req.body);
+    // console.log("사용자의 아이디는 : ", userID);
+    // console.log("사용자의 비빌번호는 : ", userPW);
+
+    res.json("로그인에 성공하셨습니다.");
 });
 
 app.get('/addRouter',function(req,res){
