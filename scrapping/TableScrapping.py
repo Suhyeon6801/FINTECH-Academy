f4749af36926ac5b008c21f4c19011d48873662f
Suhyeon6801 <k0s3y0@naver.com>
2020-07-22 11:47:07 +0900

    Table 스크래핑test

---
+++ scrapping/TableScrapping.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+driver = webdriver.Chrome('./chromedriver')
+driver.implicitly_wait(3)
+driver.get(
+    'https://www.alimi.or.kr/api/a/vacant/selectApiVacant.do')
+
+table = driver.find_element_by_class_name('search_list')
+tableBody = table.find_element_by_tag_name('tbody') #id가 따로 없으면 tag_name, 여러개가 반복 되면 elements
+rows = tableBody.find_elements_by_tag_name('tr')
+
+for index, value in enumerate(rows):
+    if index != 0:
+        rowNo = value.find_elements_by_tag_name('td')[1]
+        print(rowNo.text)
+        
